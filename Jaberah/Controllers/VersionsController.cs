537c5d5c498d5eaf4fb2b8ebb0b4dd2ed3b0ffac
Mohamed Saeed bin Omar <131279191+MohamedSaeed-dev@users.noreply.github.com>
2025-03-21 05:48:39 +0300

    Cache version

--- Jaberah/Controllers/VersionsController.cs
+++ Jaberah/Controllers/VersionsController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using static Jaberah.Models.DTOs.Login;
 
 namespace Jaberah.Controllers
