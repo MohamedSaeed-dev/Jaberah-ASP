a5bfbb52d8e4f6a64f0095647c75741096b7983d
MohamedSaeed-dev <mohamedsas966@gmail.com>
2025-03-23 00:22:32 +0300

    send notif when new version

--- Jaberah/Controllers/VersionsController.cs
+++ Jaberah/Controllers/VersionsController.cs
@@ -1,53 +1,40 @@
-﻿using Google.Api.Gax;
+﻿using FirebaseAdmin.Messaging;
+using Google.Api.Gax;
 using Jaberah.Models.DTOs;
 using Jaberah.Models.MyDbContext;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Caching.Memory;
 using static Jaberah.Models.DTOs.Login;
 
 namespace Jaberah.Controllers
 {
     [Route("api/versions")]
     [ApiController]
-    public class VersionsController(JaberahDBContext db, IMemoryCache cache) : ControllerBase
+    public class VersionsController(JaberahDBContext db) : ControllerBase
     {
         private readonly JaberahDBContext _db = db;
-        private readonly IMemoryCache _cache = cache;
-        
         [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetLastVersion([FromQuery] string version)
         {
-            if (!_cache.TryGetValue("appVer", out var appVer))
-{
-    var appVersion = await _db.Versions.FirstOrDefaultAsync();
+            var appVersion = await _db.Versions
+            .FirstOrDefaultAsync();
 
-    if (appVersion == null) 
-        return NotFound(new { message = "version not found" });
+            if (appVersion == null) return NotFound(new { message = "version not found" });
 
-    bool isUpdateRequired = CompareVersions(version, appVersion.MinRequiredVersion) < 0;
-    bool isUpdateAvailable = CompareVersions(version, appVersion.LatestVersion) < 0;
+            bool isUpdateRequired = CompareVersions(version, appVersion.MinRequiredVersion) < 0;
+            bool isUpdateAvailable = CompareVersions(version, appVersion.LatestVersion) < 0;
 
-    appVer = new
-    {
-        latestVersion = appVersion.LatestVersion,
-        minRequiredVersion = appVersion.MinRequiredVersion,
-        isUpdateRequired,
-        isUpdateAvailable,
-        url = appVersion.URL
-    };
-
-    _cache.Set("appVer", appVer, new MemoryCacheEntryOptions
-    {
-        AbsoluteExpirationRelativeToNow = TimeSpan.FromDays(7),
-        SlidingExpiration = TimeSpan.FromHours(12)
-    });
-}
-
-return Ok(appVer);
+            return Ok(new
+            {
+                latestVersion = appVersion.LatestVersion,
+                minRequiredVersion = appVersion.MinRequiredVersion,
+                isUpdateRequired,
+                isUpdateAvailable,
+                url = appVersion.URL
+            });
         }
         [AllowAnonymous]
         [HttpPut]
@@ -56,20 +43,59 @@ return Ok(appVer);
             if (string.IsNullOrWhiteSpace(version) || string.IsNullOrWhiteSpace(url)) return BadRequest(new {message = "invalid data"}); 
             var lastVersion = await _db.Versions.FirstOrDefaultAsync();
             if (lastVersion == null) return NotFound(new { message = "version not found" });
-            lastVersion.LatestVersion = version;
-            lastVersion.URL = url.Contains("dl=0") ? url.Replace("dl=0", "dl=1") : lastVersion.URL;
-            var versionParts = version.Split('.').Select(int.Parse).ToArray();
-            var requiredParts = lastVersion.MinRequiredVersion.Split('.').Select(int.Parse).ToArray();
-            if (versionParts[0] >  requiredParts[0])
+
+
+            var versionParts = version.Split('.').Select(int.Parse).ToList();
+            var requiredParts = lastVersion.MinRequiredVersion.Split('.').Select(int.Parse).ToList();
+
+            while (versionParts.Count < 3) versionParts.Add(0);
+            while (requiredParts.Count < 3) requiredParts.Add(0);
+
+            if (versionParts[0] > requiredParts[0] || versionParts[0] < requiredParts[0])  // Major version increased
             {
                 lastVersion.MinRequiredVersion = version;
             }
-            else if (versionParts[1]  > requiredParts[1])
+            else if (versionParts[1] > requiredParts[1] || versionParts[1] < requiredParts[1])  // Minor version increased
+            {
+                lastVersion.MinRequiredVersion = version;
+            }
+            else if (versionParts[2] > requiredParts[2] || versionParts[2] < requiredParts[2])  // Patch version increased
             {
                 lastVersion.MinRequiredVersion = $"{versionParts[0]}.{versionParts[1]}.0";
             }
+
+
+            if (lastVersion.LatestVersion != version)
+            {
+                var messageBuilder = new Message()
+                {
+                    Notification = new FirebaseAdmin.Messaging.Notification()
+                    {
+                        Title = "تحديث جديد",
+                        Body = $"{version}",
+                    },
+                    Data = new Dictionary<string, string>
+                    {
+                        { "topic", "newVersion" },
+                        { "version", version },
+                        { "url", url },
+                        { "minRequired",lastVersion.MinRequiredVersion }
+                    },
+                    Topic = "newVersion"
+                };
+                try
+                {
+                    string response = await FirebaseMessaging.DefaultInstance.SendAsync(messageBuilder);
+                }
+                catch
+                {
+                    return StatusCode(500, new { message = "حدث خطأ في ارسال الاشعار" });
+                }
+            }
+
+            lastVersion.LatestVersion = version;
+            lastVersion.URL = url.Contains("dl=0") ? url.Replace("dl=0", "dl=1") : lastVersion.URL;
             await _db.SaveChangesAsync();
-            _cache.Remove("appVer");
             return Ok(lastVersion);
         }
         [NonAction]
