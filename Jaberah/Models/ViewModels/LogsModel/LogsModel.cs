a0700bd405fbff3442fcb3d3690ff2ac1e10043b
Mohamed-Sa2024 <mohammedsaeed@cleanlife.sa>
2025-04-09 08:18:05 +0300

    fix logs deserailize

--- Jaberah/Models/ViewModels/LogsModel/LogsModel.cs
+++ Jaberah/Models/ViewModels/LogsModel/LogsModel.cs
@@ -1,22 +1,44 @@
+using System;
+using System.Text.Json.Serialization;
+
 public class LogEntry
 {
+    [JsonPropertyName("timestamp")]
     public DateTime Timestamp { get; set; }
+
+    [JsonPropertyName("level")]
     public string Level { get; set; }
+
+    [JsonPropertyName("messageTemplate")]
     public string MessageTemplate { get; set; }
+
+    [JsonPropertyName("properties")]
     public Properties Properties { get; set; }
 }
 
 public class Properties
 {
+    [JsonPropertyName("httpLog")]
     public HttpLog HttpLog { get; set; }
 }
 
 public class HttpLog
 {
+    [JsonPropertyName("timestamp")]
     public DateTime Timestamp { get; set; }
+
+    [JsonPropertyName("method")]
     public string Method { get; set; }
+
+    [JsonPropertyName("url")]
     public string Url { get; set; }
+
+    [JsonPropertyName("body")]
     public string Body { get; set; }
+
+    [JsonPropertyName("statusCode")]
     public int StatusCode { get; set; }
+
+    [JsonPropertyName("response")]
     public string Response { get; set; }
 }
